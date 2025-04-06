e0fd9a74ddf8bc630623aad2abfd8efb882ca2b1
Ruslan Adzhigitov <ruslanadzhigitov@MacbookAirRuslan.local>
2025-04-06 14:12:53 +0300

    hm

--- core/lesson-26-container-type/index.ts
+++ core/lesson-26-container-type/index.ts
@@ -7,10 +7,14 @@ class Result<T, E extends Error> {
         try {
             this.result = executor();
             this.state = 'fulfilled';
-        } catch(e) {
+        } catch (e) {
             this.error = e;
             this.state = 'rejected';
         }
+    }
+
+    static resolve<T, E extends Error>(value: T | Result<T, E>): Result<T, E> {
+        return value instanceof Result ? value : new Result(() => value);
     }
 
     then(cb: (data: T) => void) {
@@ -32,14 +36,47 @@ class Result<T, E extends Error> {
 const res1 = new Result(() => 42);
 
 res1.then((data) => {
-  console.log(data);
+    console.log(data);
 });
 
 const res2 = new Result(() => { throw 'Boom!'; });
 
 res1.then((data) => {
-  // Этот callback не вызовется
-  console.log(data);
+    // Этот callback не вызовется
+    console.log(data);
 
-// А этот вызовется
+    // А этот вызовется
 }).catch(console.error);
+
+function exec(gen: () => Generator<Result<any, any>>) {
+    const iterator = gen();
+    let data: any;
+    let returnValue: any;
+
+    while (true) {
+        if (returnValue) return returnValue;
+
+        const { value, done } = iterator.next(data);
+        const resolved = Result.resolve(value);
+
+        if (done) return resolved;
+
+        resolved.then((value) => {
+            data = value;
+        }).catch((err) => {
+            returnValue = new Result(() => { throw err; });
+        });
+    }
+}
+
+exec(function* main() {
+    const res1 = new Result(() => 42);
+    console.log(yield res1);
+
+    try {
+        const res2 = yield new Result(() => { throw 'Boom!'; });
+
+    } catch (err) {
+        console.error(err);
+    }
+});
